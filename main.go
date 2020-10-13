c6b20706896e09b1a8451d0d61c1c93809680d2d
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2020-10-13 13:18:24 -0700

    Use Ingress to demonstrate that Status update skips the rate limiter

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/extensions/v1beta1"
+
 	"github.com/pkg/errors"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	controllerruntime "sigs.k8s.io/controller-runtime"
@@ -47,8 +51,8 @@ func main() {
 	// Create the controller, and register it with the manager
 	mlog.Info("creating controller")
 	if _, err := controllerruntime.NewControllerManagedBy(mgr).
-		// Reconcile v1.ConfigMaps.
-		For(&corev1.ConfigMap{}).
+		// Reconcile v1beta1.Ingress.
+		For(&v1beta1.Ingress{}).
 		// But include only those with the right annotation
 		// (Server-side filtering will be available in the future, see https://github.com/kubernetes-sigs/controller-runtime/issues/244)
 		WithEventFilter(predicate.NewPredicateFuncs(func(meta metav1.Object, object runtime.Object) bool {
@@ -72,9 +76,7 @@ func main() {
 // created/updated/deleted, or processed after a requeue.
 func reconcileFunc(r reconcile.Request) (reconcile.Result, error) {
 	// Setup logger
-	rlog := globalLog.WithValues("kind", corev1.ConfigMap{}.Kind, "name", r.Name, "namespace", r.Namespace)
-	rlog.Info("started reconcile")
-	defer rlog.Info("finished reconcile")
+	rlog := globalLog.WithValues("kind", v1beta1.Ingress{}.Kind, "name", r.Name, "namespace", r.Namespace)
 
 	// Setup context
 	ctx, cancel := context.WithTimeout(context.Background(), reconcileTimeout)
@@ -84,10 +86,20 @@ func reconcileFunc(r reconcile.Request) (reconcile.Result, error) {
 	c := mgr.GetClient()
 
 	// Get the object being reconciled
-	cm := corev1.ConfigMap{}
-	err := c.Get(ctx, r.NamespacedName, &cm)
-	if err != nil {
+	rlog.Info("getting object")
+	ing := &v1beta1.Ingress{}
+	if err := c.Get(ctx, r.NamespacedName, ing); err != nil {
 		return reconcile.Result{}, errors.Wrap(err, "unable to get object")
+	}
+
+	rlog.Info("updating status")
+	ing.Status.LoadBalancer.Ingress = []v1.LoadBalancerIngress{
+		{
+			Hostname: fmt.Sprintf("host%d.example.com", rand.Int()),
+		},
+	}
+	if err := c.Status().Update(ctx, ing); err != nil {
+		return reconcile.Result{}, errors.Wrap(err, "unable to update status")
 	}
 
 	return reconcile.Result{}, nil
